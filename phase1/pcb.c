dbb88503cc88463d6a2c18fcfa58f2218c760175
Andrea <andrea.guidotti,2003@gmail.com>
2024-12-04 21:14:48 +0100

    Funzioni HeadProcQ,RemoveProcQ,OutProcQ,EmptyChild

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -23,15 +23,36 @@ void insertProcQ(struct list_head* head, pcb_t* p) {
 }
 
 pcb_t* headProcQ(struct list_head* head) {
+    if (list_empty(head))
+        return NULL;
+    else{
+        return container_of(head->next,pcb_t,p_list);
+    }
 }
 
 pcb_t* removeProcQ(struct list_head* head) {
+        if (list_empty(head))
+            return NULL;
+        else {
+            pcb_t* p= container_of(head->next,pcb_t,p_list);
+            list_del(p->p_list);
+            return p;
+        }
 }
 
 pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
+    struct list_head* pos;
+    list_for_each(pos,head){
+        if (container_of(pos,pcb_t,p_list)==p){
+            list_del(p->p_list);
+            return p;
+        }
+    }
+    return NULL;
 }
 
 int emptyChild(pcb_t* p) {
+    return (list_empty(p->p_child)))
 }
 
 void insertChild(pcb_t* prnt, pcb_t* p) {
