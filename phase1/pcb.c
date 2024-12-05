68d9802838880f64e966b38c4eed2b2ba5617471
Andrea <andrea.guidotti,2003@gmail.com>
2024-12-05 15:47:17 +0100

    Commentato il codice e aggiunto EmptyprocQ

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -26,7 +26,9 @@ void mkEmptyProcQ(struct list_head* head) {
     INIT_LIST_HEAD(head);
 }
 
+//Verifica se una coda è vuota
 int emptyProcQ(struct list_head* head) {
+    return list_empty(head);
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p) {
@@ -39,35 +41,39 @@ void insertProcQ(struct list_head* head, pcb_t* p) {
     head->prev=&p->p_list;
 }
 
+//Restituisce il primo PCB dalla coda dei processi.
 pcb_t* headProcQ(struct list_head* head) {
-    if (list_empty(head))
+    if (list_empty(head)) // Verifica se la lista è vuota.
         return NULL;
     else{
-        return container_of(head->next,pcb_t,p_list);
+        return container_of(head->next,pcb_t,p_list); //Restituisce il puntatore al primo elemento successivo a head
     }
 }
 
+//Rimuove il PCB in testa alla lista
 pcb_t* removeProcQ(struct list_head* head) {
-        if (list_empty(head))
+        if (list_empty(head)) // Verifica se la lista è vuota.
             return NULL;
         else {
-            pcb_t* p= container_of(head->next,pcb_t,p_list);
-            list_del(&p->p_list);
-            return p;
+            pcb_t* p= container_of(head->next,pcb_t,p_list); //Restituisce il puntatore al primo elemento successivo a head
+            list_del(&p->p_list); //Rimuove il puntatore dalla lista
+            return p; //Ritorno la lista
         }
 }
 
+//Rimuove un PCB specifico dalla coda
 pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
     struct list_head* pos;
-    list_for_each(pos,head){
-        if (container_of(pos,pcb_t,p_list)==p){
-            list_del(&p->p_list);
-            return p;
+    list_for_each(pos,head){   //Ciclo la lista
+        if (container_of(pos,pcb_t,p_list)==p){   //Vedo se il PCB trovato è uguale a quello specifico
+            list_del(&p->p_list); //lo rimuovo dalla lista
+            return p;   //Ritorno la lista con il PCB rimosso
         }
     }
     return NULL;
 }
 
+//verifico se un Processo ha figli
 int emptyChild(pcb_t* p) {
     return (list_empty(&p->p_child));
 }
