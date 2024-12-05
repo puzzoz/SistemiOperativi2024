fe2e8f59d6376cf3e392e373838b131047eb13f6
pleaseddd <147441873+pleaseddd@users.noreply.github.com>
2024-12-05 20:13:02 +0100

    Add files via upload
    aggiunte funzioni

---
+++ pcb.c
@@ -0,0 +1,160 @@
+#include "./headers/pcb.h"
+
+static struct list_head pcbFree_h;
+static pcb_t pcbFree_table[MAXPROC];
+static int next_pid = 1;
+
+void initPcbs() {
+    //inizializza la pcbFree list, contiene tutti gli elementi dell'array statico MAXPROC PCBs
+    for(int i=0; i<MAXPROC; i++){
+        INIT_LIST_HEAD(&pcbFree_table[i].p_list);   //inizializzo il nodo p_list (inizializza la testa della lista circolare)
+        list_add_tail(&pcbFree_table[i], &pcbFree_h);
+    }
+}
+
+void freePcb(pcb_t* p) {
+    //inserisce l'elemento puntato da p sulla lista pcbFree
+    //usata dai pcbs che non vengono piu' usati
+    list_add_tail(&p->p_list, &pcbFree_h);
+}
+
+pcb_t* allocPcb() {
+    if (list_empty(&pcbFree_h)) return NULL; //se la lista di PCB disponibili è vuota ritorna NULL
+    else {
+        //recupera il primo PCB libero della lista, prende il puntatore alla struttura pcb_t del primo elemento della lista
+        pcb_t *p = container_of((&pcbFree_h)->next, pcb_t, p_list);
+        //rimuove il primo elemento della lista dei PCB liberi, aggiornando poi la lista
+        list_del((&pbcFree_h)->next);
+
+        //inizializzo la coda dei processi child e siblings
+        INIT_LIST_HEAD(&(p->p_list)); //prima inizializzo p_list come lista vuota
+        INIT_LIST_HEAD(&(p->p_child)); //poi inizializzo la lista dei figli
+
+        //imposto il padre del PCB a NULL perchè è il primo PCB
+        p->parent = NULL;
+        //inizializzo la lista dei sibling
+        INIT_LIST_HEAD(&(p->p_sib));
+
+        //inizializzo i campi di stato del processo, azzero i registri e gli stati
+        p->p_s.entry_hi = 0;    //interruzioni o eccezioni
+        p->p_s.cause = 0;       //cause memorizza il motivo di un'interruzione
+        p->p_s.status = 0;      //stato processo (in esecuzione, terminato, sospeso etc)
+        p->p_s.pc_epc = 0;      //program counter e exeption p.c.
+
+        for (int i = 0; i < sizeof(p->s.gpr) / sizeof(p->p_s.gpr[0]); i++){
+            p->p_s.gpr[i] = 0;  //azzero i registri generali del processo
+        }
+
+        //registi usati per lo operazioni sui numeri a 64 bit
+        p->p_s.hi = 0;
+        p->p_s.lo = 0;
+        //inizializzo il contatore del tempo
+        p->p_time = 0; //tiene traccia del tempo di escuzione del processo
+        //inizializzo la lista della coda dei messaggi in ingresso
+        INIT_LIST_HEAD(&(p->msg_inbox));
+        //imposto il puntatore alla struttura di supporto a NULL
+        p->p_supportStruct = NULL;
+        //imposto il process id a 0, poichè primo
+        p->p_pid = 0;
+        //numero dispostivo generico (nessun dispotivio)
+        p->dev_no = -1;
+        p->from = 0;
+        return p;
+    }
+
+}
+
+void mkEmptyProcQ(struct list_head* head) {
+    // metodo usato per inizializzare una variabile per essere un head pointer ad un processo queue
+    INIT_LIST_HEAD(head);
+}
+
+//Verifica se una coda è vuota
+int emptyProcQ(struct list_head* head) {
+    return list_empty(head);
+}
+
+void insertProcQ(struct list_head* head, pcb_t* p) {
+    //inserisce il PCB puntato da p nella coda dei processi, la quale testa e' puntata da head
+    struct list_head* last=head->prev;  //trovo l'ultimo nodo della lista
+    //aggiorno i puntatori
+    p->p_list.next=head;    //il prossimo nodo di p e' head
+    p->p_list.prev=last;    //il nodo precedene di p e' last
+    last->next=&p->p_list;  //aggiorno last e head
+    head->prev=&p->p_list;
+}
+
+//Restituisce il primo PCB dalla coda dei processi.
+pcb_t* headProcQ(struct list_head* head) {
+    if (list_empty(head)) // Verifica se la lista è vuota.
+        return NULL;
+    else{
+        return container_of(head->next,pcb_t,p_list); //Restituisce il puntatore al primo elemento successivo a head
+    }
+}
+
+//Rimuove il PCB in testa alla lista
+pcb_t* removeProcQ(struct list_head* head) {
+        if (list_empty(head)) // Verifica se la lista è vuota.
+            return NULL;
+        else {
+            pcb_t* p= container_of(head->next,pcb_t,p_list); //Restituisce il puntatore al primo elemento successivo a head
+            list_del(&p->p_list); //Rimuove il puntatore dalla lista
+            return p; //Ritorno la lista
+        }
+}
+
+//Rimuove un PCB specifico dalla coda
+pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
+    struct list_head* pos;
+    list_for_each(pos,head){   //Ciclo la lista
+        if (container_of(pos,pcb_t,p_list)==p){   //Vedo se il PCB trovato è uguale a quello specifico
+            list_del(&p->p_list); //lo rimuovo dalla lista
+            return p;   //Ritorno la lista con il PCB rimosso
+        }
+    }
+    return NULL;
+}
+
+//verifico se un Processo ha figli
+int emptyChild(pcb_t* p) {
+    //l'output è il risultato della verifica di list empty che prende come 
+    //parametro l'indirzzo (con &) della testa della lista p_child puntata da p
+    return (list_empty(&p->p_child));
+    //true (1) se vuota, false (0) altrimenti
+}
+
+void insertChild(pcb_t* prnt, pcb_t* p) {
+    //per prima cosa setto prnt come genitore di p
+    p->parent = prnt; 
+    //se la lista dei figli è vuota
+    if (emptyChild(prnt))
+        //aggiungo semplicemnte come primo elemento (linkando i siblings se ci sono)
+        list_add(&(p->p_sib), &(prnt->p_child));
+    //altrimenti
+    else
+        //aggiungo alla fine, preservando la FIFO (linkando sempre i siblings)
+        list_add_tail(&(p->p_sib), &(prnt->p_child));
+}
+
+pcb_t* removeChild(pcb_t* p) {
+    //se la lista dei figli è vuota non devo togliere nulla
+    if (emptyChild(p)) return NULL;
+    else {
+        //risalgo al primo figlio utilizando i fratelli 
+        pcb_t *first_child = container_of(list_next(&(p->p_child)), pcb_t, p_sib);
+        list_del(&(first_child->p_sib)) //rimuovo dalla lista dei siblings
+        first_child->p_parent = NULL //elimino il puntatore dal figlio al padre
+        
+        return first_child //ritorno il child eliminato
+    }
+}
+
+pcb_t* outChild(pcb_t* p) {
+    if (p->p_prnt == NULL) return NULL;
+    else {
+        list_del(&(p->p_sib)); //rimuovo dalla lista dei siblings
+        p->p_parent = NULL; //elimino il puntatore dal figlio al padre
+        return p //ritorno il p
+    }
+}
