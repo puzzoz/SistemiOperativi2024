cba1bdcf5ab81fda1c96db670bd0d12c7d83166c
EricManara0 <ericmanara@gmail.com>
2024-12-22 14:13:12 +0100

    outBlocked now removes semaphores if their process queue is empty + fix docs

--- phase1/asl.c
+++ phase1/asl.c
@@ -11,7 +11,7 @@ static struct list_head semd_h;
 #define for_each_semd(head) semd_t *curr; list_for_each_entry(curr, head, s_link)
 
 /*
-    ritorna il semaforo con key contenuta in semAdd, NULL se non lo trova
+ * ritorna il semaforo con key contenuta in semAdd, NULL se non lo trova
 */
 semd_t *findSem(const int *semAdd) {
     invariant(semAdd)
@@ -22,8 +22,8 @@ semd_t *findSem(const int *semAdd) {
 }
 
 /*
-    ritorna il processo con id p_id contenuto nella lista dei processi bloccati
-    dal semaforo con key in semAdd, NULL altrimenti
+ * ritorna il processo con id p_id contenuto nella lista dei processi bloccati
+ * dal semaforo con key in semAdd, NULL altrimenti
 */
 pcb_t *findPcb(const int *semAdd, int p_id) {
     invariant(semAdd)
@@ -37,8 +37,8 @@ pcb_t *findPcb(const int *semAdd, int p_id) {
 }
 
 /*
-    inizializza la lista dei semafori liberi semdFree_h allocando MAXPROC
-    semafori dall'array semd_table
+ * inizializza la lista dei semafori liberi semdFree_h allocando MAXPROC
+ * semafori dall'array semd_table
 */
 void initASL() {
     INIT_LIST_HEAD(&semdFree_h);
@@ -47,12 +47,13 @@ void initASL() {
         list_add_tail(&semd_table[i].s_link, &semdFree_h);
     }
 }
+
 /*
-    inserisce il processo p tra i processi bloccati dal semaforo con chiave
-    in semAdd. se il semaforo non viene trovato in semd_h e la lista dei semafori
-    liberi semdFree_h non è vuota, un semaforo viene spostato in semd_h e il
-    processo viene aggiunto. se semdFree_h è piena e il processo non viene aggiunto
-    ritorna TRUE, FALSE altrimenti
+ * inserisce il processo p tra i processi bloccati dal semaforo con chiave
+ * in semAdd. se il semaforo non viene trovato in semd_h e la lista dei semafori
+ * liberi semdFree_h non è vuota, un semaforo viene spostato in semd_h e il
+ * processo viene aggiunto. se semdFree_h è piena e il processo non viene aggiunto
+ * ritorna TRUE, FALSE altrimenti
 */
 int insertBlocked(int* semAdd, pcb_t* p) {
     semd_t *sem = findSem(semAdd);
@@ -72,23 +73,30 @@ int insertBlocked(int* semAdd, pcb_t* p) {
 }
 
 /*
-    rimuove il primo processo bloccato dal semaforo con key in semAdd.
-    se il semaforo non ha altri processi viene spostato nei semafori liberi
+ * se il semaforo sem non ha processi bloccati viene spostato in semdFree_h
+ */
+void freeSemIfEmpty(semd_t *sem) {
+    if (emptyProcQ(&sem->s_procq)) {
+        list_del(&sem->s_link);
+        list_add_tail(&sem->s_link, &semdFree_h);
+    }
+}
+
+/*
+ * rimuove il primo processo bloccato dal semaforo con key in semAdd.
+ * se il semaforo non ha altri processi viene spostato nei semafori liberi
 */
 pcb_t* removeBlocked(int* semAdd) {
     semd_t *sem = findSem(semAdd);
     invariant(sem)
     pcb_t *p = removeProcQ(&sem->s_procq);
-    if (emptyProcQ(&sem->s_procq)) {
-        list_del(&sem->s_link);
-        list_add_tail(&sem->s_link, &semdFree_h);
-    }
+    freeSemIfEmpty(sem);
     return p;
 }
 
 /*
-    rimuove e ritorna il processo con id pid dal semaforo in cui è
-    bloccato, NULL altrimenti
+ * rimuove e ritorna il processo con id pid dal semaforo in cui è
+ * bloccato, NULL altrimenti
 */
 pcb_t* outBlockedPid(int pid) {
     struct list_head *currSem = &semd_h; pcb_t *pcb;
@@ -96,25 +104,28 @@ pcb_t* outBlockedPid(int pid) {
         pcb = findPcb(getSem(currSem)->s_key, pid);
         currSem = list_next(currSem);
     }
-    return outProcQ(&getSem(currSem)->s_procq, pcb);
+    pcb_t *removed = outProcQ(&getSem(currSem)->s_procq, pcb);
+    freeSemIfEmpty(getSem(currSem));
+    return removed;
 }
 
 /*
-    rimuove e ritorna il processo p dal semaforo in cui è bloccato,
-    NULL altrimenti
+ * rimuove e ritorna il processo p dal semaforo in cui è bloccato,
+ * NULL altrimenti
 */
 pcb_t* outBlocked(pcb_t* p) {
     invariant(p)
     for_each_semd(&semd_h) {
         pcb_t *out = outProcQ(&curr->s_procq, p);
+        freeSemIfEmpty(curr);
         if (out != NULL) return out;
     }
     return NULL;
 }
 
 /*
-    ritorna il primo processo bloccato dal semaforo con key in semAdd,
-    NULL altrimenti
+ * ritorna il primo processo bloccato dal semaforo con key in semAdd,
+ * NULL altrimenti
 */
 pcb_t* headBlocked(int* semAdd) {
     semd_t *sem = findSem(semAdd);
