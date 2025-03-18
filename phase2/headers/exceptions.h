0f675aded36f44f2dc07faa5a9745791dadf8611
EricManara0 <ericmanara@gmail.com>
2025-03-18 15:16:03 +0100

    fix uTLB_RefillHandler includes

--- phase2/headers/exceptions.h
+++ phase2/headers/exceptions.h
@@ -1,6 +1,9 @@
 #ifndef MULTIPANDOS_EXCEPTIONS_H
 #define MULTIPANDOS_EXCEPTIONS_H
 
+#include <uriscv/liburiscv.h>
+#include "../../headers/types.h"
+
 void uTLB_RefillHandler() {
     int prid = getPRID();
     setENTRYHI(0x80000000);
