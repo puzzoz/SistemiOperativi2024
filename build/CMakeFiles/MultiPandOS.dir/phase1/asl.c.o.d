0a517c39b963347fdddee49d2e1a3d0fdb976213
EricManara0 <ericmanara@gmail.com>
2025-03-18 15:13:42 +0100

    adds build directories in .gitignore

--- build/CMakeFiles/MultiPandOS.dir/phase1/asl.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/MultiPandOS.dir/phase1/asl.c.o: \
- /mnt/c/Users/mtito/OneDrive/Desktop/appunti\ uni/secondo\ anno/sistemi\ operativi/SistemiOperativi2024/phase1/asl.c \
- /mnt/c/Users/mtito/OneDrive/Desktop/appunti\ uni/secondo\ anno/sistemi\ operativi/SistemiOperativi2024/phase1/./headers/pcb.h \
- /mnt/c/Users/mtito/OneDrive/Desktop/appunti\ uni/secondo\ anno/sistemi\ operativi/SistemiOperativi2024/phase1/./headers/../../headers/listx.h \
- /mnt/c/Users/mtito/OneDrive/Desktop/appunti\ uni/secondo\ anno/sistemi\ operativi/SistemiOperativi2024/phase1/./headers/../../headers/types.h \
- /usr/include/uriscv/types.h \
- /mnt/c/Users/mtito/OneDrive/Desktop/appunti\ uni/secondo\ anno/sistemi\ operativi/SistemiOperativi2024/phase1/./headers/../../headers/./const.h \
- /usr/include/uriscv/const.h /usr/include/uriscv/csr.h \
- /mnt/c/Users/mtito/OneDrive/Desktop/appunti\ uni/secondo\ anno/sistemi\ operativi/SistemiOperativi2024/phase1/./headers/../../headers/./listx.h
--- phase2/headers/exceptions.h
+++ phase2/headers/exceptions.h
@@ -1,4 +1,12 @@
 #ifndef MULTIPANDOS_EXCEPTIONS_H
 #define MULTIPANDOS_EXCEPTIONS_H
 
-#endif //MULTIPANDOS_EXCEPTIONS_H
+void uTLB_RefillHandler() {
+    int prid = getPRID();
+    setENTRYHI(0x80000000);
+    setENTRYLO(0x00000000);
+    TLBWR();
+    LDST(GET_EXCEPTION_STATE_PTR(prid));
+}
+
+#endif
