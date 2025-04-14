570dead98f7bd1cad76e04811cf8aab21e30f0f0
EricManara0 <eric.manara@studio.unibo.it>
2025-04-14 15:27:50 +0200

    fix memset

--- phase2/initial.c
+++ phase2/initial.c
@@ -14,6 +14,7 @@
 
 static int processCount = 0; //num di processi iniziati ma non ancora terminati
 
+typedef unsigned int size_t;
 __attribute__((unused)) void memset(void *dest, int value, size_t n)
 {
     for (size_t i = 0; i < n; i++)
