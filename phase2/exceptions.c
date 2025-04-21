c05b2927261e197e0acc968ac74b6b05433493b7
EricManara0 <ericmanara@gmail.com>
2025-04-21 19:20:08 +0200

    refactoring syscalls

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -10,7 +10,6 @@
 #include <uriscv/cpu.h>
 
 #define EXCEPTION_IN_KERNEL_MODE(excStatus) (excStatus->status & MSTATUS_MPP_MASK)
-#define EXC_RETURN(excState, x) {excState->reg_a0 = x; break;}
 #define CAUSE_GET_EXCCODE(x) ((x)&CAUSE_EXCCODE_MASK)
 
 
@@ -69,89 +68,117 @@ void interruptExcHandler() { dispatchInterrupt(getCAUSE(), GET_EXCEPTION_STATE_P
 
 void programTrapExcHandler() { passUpOrDie(GENERALEXCEPT); }
 
+void createProcess(state_t *excState) {
+    MUTEX_GLOBAL(
+        pcb_t *curr_p = *current_process();
+        pcb_t *new_p = allocPcb();
+        if (new_p == NULL) {
+            excState->reg_a0 = -1;
+        } else {
+            // aggiunge il nuovo processo all'albero dei processi
+            insertProcQ(&curr_p->p_list, new_p);
+            insertChild(curr_p, new_p);
+            // aggiunge il processo alla ready queue
+            insertProcQ(ready_queue(), new_p);
+            (*process_count())++;
+        }
+    )
+    new_p->p_s = *((state_t *) excState->reg_a1);
+    new_p->p_supportStruct = (support_t *) excState->reg_a3;
+    excState->reg_a0 = new_p->p_pid;
+}
+void termProcess() {
+    MUTEX_GLOBAL(
+        freePcb(*current_process());
+        (*process_count())--;
+    )
+}
+void passeren(state_t *excState) {
+    semd_t *sem = ((semd_t *) excState->reg_a1);
+    unsigned int callScheduler = 0;
+    MUTEX_GLOBAL(
+            if (*sem->s_key > 0) {
+                (*sem->s_key)--;
+            } else {
+                blockPcb(sem->s_key, *current_process(), excState);
+                callScheduler = 1;
+            }
+    )
+    if (callScheduler) scheduler();
+}
+void verhogen(state_t *excState) {
+    semd_t *sem = ((semd_t *) excState->reg_a1);
+    unsigned int callScheduler = 0;
+    MUTEX_GLOBAL(if (*sem->s_key < 1) {
+        (*sem->s_key)++;
+    } else {
+        blockPcb(sem->s_key, *current_process(), excState);
+        callScheduler = 1;
+    })
+    if (callScheduler) scheduler();
+}
+void doio(state_t *excState) {
+    //blocco il processo sul semaforo a cui fa riferimento commandAddr
+    MUTEX_GLOBAL(
+        pcb_t *curr_p = *current_process();
+        curr_p->p_semAdd = (int *) excState->reg_a1;
+        blockPcb((int *) excState->reg_a1, curr_p, excState);
+    )
+    //Scrivo il comando nel Registro
+    excState->reg_a1 = excState->reg_a2;
+    scheduler();
+}
+void getTime(state_t *excState) {
+    MUTEX_GLOBAL(
+        excState->reg_a0 = (*current_process())->p_time
+    )
+}
+void clockWait(state_t *excState) {
+    MUTEX_GLOBAL(
+        blockPcb(&clock_sem, *current_process(), excState);
+    )
+    scheduler();
+}
+void getSupportPointer(state_t *excState) {
+    MUTEX_GLOBAL(
+        excState->reg_a0 = (unsigned int) (*current_process())->p_supportStruct
+    )
+}
+void getProcessId(state_t *excState) {
+    MUTEX_GLOBAL(
+        pcb_t *curr_pr = *current_process();
+        excState->reg_a0 = (excState->reg_a1 == 0) ?
+            curr_pr->p_pid
+            : (curr_pr->p_parent != NULL) ? curr_pr->p_parent->p_pid : 0
+    )
+}
+
 void syscallExcHandler() {
     state_t* excState = GET_EXCEPTION_STATE_PTR(getPRID());
     if (excState->reg_a0 <= 0) {
         // negative SYSCALL
         if (EXCEPTION_IN_KERNEL_MODE(excState)) {
             switch (excState->reg_a0) {
-                case CREATEPROCESS: {
-                    MUTEX_GLOBAL(
-                            pcb_t *curr_p = *current_process();
-                            pcb_t *new_p = allocPcb();
-                            if (new_p == NULL) EXC_RETURN(excState, -1)
-                            // aggiunge il nuovo processo all'albero dei processi
-                            insertProcQ(&curr_p->p_list, new_p);
-                            insertChild(curr_p, new_p);
-                            // aggiunge il processo alla ready queue
-                            insertProcQ(ready_queue(), new_p);
-                            (*process_count())++;
-                    )
-                    new_p->p_s = *((state_t *) excState->reg_a1);
-                    new_p->p_supportStruct = (support_t *) excState->reg_a3;
-                    EXC_RETURN(excState, new_p->p_pid)
-                }
+                case CREATEPROCESS:
+                    createProcess(excState); break;
                 case TERMPROCESS:
-                MUTEX_GLOBAL(
-                        freePcb(*current_process());
-                        (*process_count())--;
-                )
-                    break;
-                case PASSEREN: {
-                    semd_t *sem = ((semd_t *) excState->reg_a1);
-                    unsigned int callScheduler = 0;
-                    MUTEX_GLOBAL(if (*sem->s_key > 0) {
-                        (*sem->s_key)--;
-                    } else {
-                        blockPcb(sem->s_key, *current_process(), excState);
-                        callScheduler = 1;
-                    })
-                    if (callScheduler) scheduler();
-                    break;
-                }
-                case VERHOGEN: {
-                    semd_t *sem = ((semd_t *) excState->reg_a1);
-                    unsigned int callScheduler = 0;
-                    MUTEX_GLOBAL(if (*sem->s_key < 1) {
-                        (*sem->s_key)++;
-                    } else {
-                        blockPcb(sem->s_key, *current_process(), excState);
-                        callScheduler = 1;
-                    })
-                    if (callScheduler) scheduler();
-                    break;
-                }
+                    termProcess(); break;
+                case PASSEREN:
+                    passeren(excState); break;
+                case VERHOGEN:
+                    verhogen(excState); break;
                 case DOIO:
-                    //blocco il processo sul semaforo a cui fa riferimento commandAddr
-                MUTEX_GLOBAL(
-                        pcb_t *curr_p = *current_process();
-                        curr_p->p_semAdd = (int *) excState->reg_a1;
-                        blockPcb((int *) excState->reg_a1, curr_p, excState);
-                )
-                    //Scrivo il comando nel Registro
-                    excState->reg_a1 = excState->reg_a2;
-                    scheduler();
-                    break;
+                    doio(excState); break;
                 case GETTIME:
-                MUTEX_GLOBAL(
-                        EXC_RETURN(excState, (*current_process())->p_time))
-                case CLOCKWAIT: {
-                    MUTEX_GLOBAL(
-                            blockPcb(&clock_sem, *current_process(), excState);
-                    )
-                    scheduler();
-                    break;
-                }
-                case GETSUPPORTPTR: EXC_RETURN(excState, (unsigned int) (*current_process())->p_supportStruct)
+                    getTime(excState); break;
+                case CLOCKWAIT:
+                    clockWait(excState); break;
+                case GETSUPPORTPTR:
+                    getSupportPointer(excState); break;
                 case GETPROCESSID:
-                MUTEX_GLOBAL(
-                        pcb_t *curr_pr = *current_process();
-                        if (excState->reg_a1 == 0) {
-                            EXC_RETURN(excState, curr_pr->p_pid)
-                        } else {
-                            EXC_RETURN(excState, (curr_pr->p_parent != NULL) ? curr_pr->p_parent->p_pid : 0)
-                        }
-                )
+                    getProcessId(excState); break;
+                default:
+                    break;
             }
         } else {
             // attempting negative SYSCALL in user mode
