6a29bfd8329fe4e5b1f3c788eccb7b7170154c52
Andrea <andrea.guidotti,2003@gmail.com>
2025-05-11 14:55:28 +0200

    fix gestione ready queue in handledeviceinterrupt

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -119,6 +119,7 @@ void handleDeviceInterrupt(int excCode, state_t *exception_state) {
             int* sem = device_semaphores(sem_index);
             pcb_t *unblocked = removeBlocked(sem);
             if (unblocked != NULL) {
+                insertProcQ(ready_queue(), unblocked);
                 softBlockCount--;
                 *sem = 0;
             }
