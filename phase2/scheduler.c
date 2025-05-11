6b2f6b648bd4d2351d79e10f653dfd27ec9ee675
Andrea <andrea.guidotti,2003@gmail.com>
2025-05-11 18:04:37 +0200

    fix scheduler

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -23,28 +23,28 @@ void updateProcessCPUTime() {
 void scheduler() {
     MUTEX_GLOBAL(
             *current_process() = removeProcQ(ready_queue());
-
+            int next = emptyProcQ(ready_queue());
     )
-        //ready queue vuota
+    if (next) { //se la ready queue e' vuota
         if (*process_count() == 0) {      //0 processi attivi --> termina
             HALT();
         } else {
-                 //ci sono processi bloccati --> attesa di interrupt
-                //CPU in WAIT --> TPR a 1
-                setTPR(1);
-                //attivazione interrupt e disattivazione PLT
-                setMIE(MIE_ALL & ~MIE_MTIE_MASK);
-                unsigned int status = getSTATUS();
-                status |= MSTATUS_MIE_MASK;
-                setSTATUS(status);
-                //valore timer al max per non causare un interrupt
-                setTIMER(MUSEC_TO_TICKS(MAXPLT));
-                WAIT();
-                //ripristina status e TPR a 0
-                setTPR(0);
+            //ci sono processi bloccati --> attesa di interrupt
+            //CPU in WAIT --> TPR a 1
+            setTPR(1);
+            //attivazione interrupt e disattivazione PLT
+            setMIE(MIE_ALL & ~MIE_MTIE_MASK);
+            unsigned int status = getSTATUS();
+            status |= MSTATUS_MIE_MASK;
+            setSTATUS(status);
+            //valore timer al max per non causare un interrupt
+            setTIMER(MUSEC_TO_TICKS(MAXPLT));
+            WAIT();
+            //ripristina status e TPR a 0
+            setTPR(0);
         }
-
+    }
     setTIMER(MUSEC_TO_TICKS(TIMESLICE)); //PLT per time slice a 5ms
-    LDST(&((*current_process())->p_s)); //stato del processore caricato dal pcb corrente
     STCK(sliceStart);
+    LDST(&((*current_process())->p_s)); //stato del processore caricato dal pcb corrente
 }
