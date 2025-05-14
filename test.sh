c92f3ea262f4c55d0e8131b3fb1d1572af2940d3
EricManara0 <ericmanara@gmail.com>
2025-05-14 17:46:53 +0200

    aggiorna script di test

--- test.sh
+++ test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 phase=3
+config=config_machine.json
 if [ -n "$1" ]
 then
 	phase=$1
 fi
-mkdir -p build && (cd build && make clean && cmake .. -DPHASE=$phase && make) && uriscv-cli
+if [ $phase -eq 3 ]
+then
+	config=config_machine_phase3.json
+	echo "making devices..."
+  (cd testers && make)
+fi
+echo "compiling tests for phase $phase..."
+mkdir -p build && (cd build && make clean ; cmake .. -DPHASE=$phase && make)
+echo "launching uriscv emulator with configuration file $config..."
+uriscv-cli $config
