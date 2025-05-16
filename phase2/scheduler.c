05e1a71ae326a2f61587d96eedc564e28fc7a61e
Andrea <andrea.guidotti,2003@gmail.com>
2025-05-16 21:44:56 +0200

    Refactoring scheduler per multicpu(teoricamente giusto)

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -21,30 +21,31 @@ void updateProcessCPUTime() {
 }
 
 void scheduler() {
-    MUTEX_GLOBAL(
-            *current_process() = removeProcQ(&readyQueue);
-            int next = emptyProcQ(&readyQueue);
-    )
-    if (next) { //se la ready queue e' vuota
-        if (processCount == 0) {      //0 processi attivi --> termina
-            HALT();
-        } else {
-            //ci sono processi bloccati --> attesa di interrupt
-            //CPU in WAIT --> TPR a 1
-            setTPR(1);
-            //attivazione interrupt e disattivazione PLT
-            setMIE(MIE_ALL & ~MIE_MTIE_MASK);
-            unsigned int status = getSTATUS();
-            status |= MSTATUS_MIE_MASK;
-            setSTATUS(status);
-            //valore timer al max per non causare un interrupt
-            setTIMER(MUSEC_TO_TICKS(MAXPLT));
-            WAIT();
-            //ripristina status e TPR a 0
-            setTPR(0);
+    while (1) {
+        pcb_t *nextProc = NULL;
+        MUTEX_GLOBAL(
+                nextProc = removeProcQ(&readyQueue);
+        )
+        if (nextProc != NULL) {
+            *current_process() = nextProc;
+            setTIMER(MUSEC_TO_TICKS(TIMESLICE));
+            STCK(sliceStart);
+            LDST(&(nextProc->p_s));
         }
+
+        if (processCount == 0) {
+            HALT();  // Tutti i processi sono morti
+        }
+        // CPU in idle
+        setTPR(1);
+        setMIE(MIE_ALL & ~MIE_MTIE_MASK);
+        unsigned int status = getSTATUS();
+        status |= MSTATUS_MIE_MASK;
+        setSTATUS(status);
+        setTIMER(MUSEC_TO_TICKS(MAXPLT));
+        WAIT();     // Quando arriva un interrupt, riprova
+        setTPR(0);
     }
-    setTIMER(MUSEC_TO_TICKS(TIMESLICE)); //PLT per time slice a 5ms
-    STCK(sliceStart);
-    LDST(&((*current_process())->p_s)); //stato del processore caricato dal pcb corrente
 }
+
+
